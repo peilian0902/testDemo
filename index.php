c10850130dafed1252ad4aab54fe33da350a7b25
SDream <37138629+peilian0902@users.noreply.github.com>
2018-03-08 15:29:25 +0800

    添加一个PHP测试文件
    添加一个PHP测试文件。。。。

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	$user['name'] = "dfdf";
+	$user['sex'] = "½÷";
+	$user['age'] = "22";
+
+
+	if (condition) {
+		# code...
+	}
+	if (condition) {
+		# code...
+	} else {
+		# code...
+	}
+	switch (variable) {
+		case 'value':
+			# code...
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+	while ( <= 10) {
+		# code...
+	}
+	foreach ($variable as $key => $value) {
+		# code...
+	}
+	
+	
+?>
